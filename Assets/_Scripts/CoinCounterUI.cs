aa353393f85fd4f4b26240537886950380fb9aed
Noah Stasuik <97553023+noahstaz@users.noreply.github.com>
2025-03-08 21:45:30 -0800

    UI updates

---
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class CoinCounterUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform coinTextContainer;
+    [SerializeField] private float duration;
+
+    private float containerInitPosition;
+    private float moveAmount;
+
+    private void Start()    
+    {
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
+    }
+
+    public void UpdateScore(int score)
+    {
+        // set the score to the masked text UI
+        toUpdate.SetText($"{score}");
+        // trigger the local move animation
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount,
+            duration);
+    }
+}
